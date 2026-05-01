1eae1f5c999f9153ca233a47cc63d1047254400f
Adrian Alvarado <a243469aa@correu.escoladeltreball.org>
2026-05-02 00:03:03 +0200

    Refactor user model and API: update user registration, login, and profile management to use first and last names, implement username generation, and enhance user data retrieval

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,28 @@
 import express from 'express'
 import {
+  getAllUsers,
   registerUser,
   loginUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers
+  updateMyUser,
+  getMyUser,
+  getUserByUsername,
+  updateMyUsername
 } from '../controllers/UserController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.get('/', protect, admin, getAllUsers)
+router.get('/getUsersForChat', protect, getAllUsers)
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.get('/', protect, admin, getUsers)
-router.get('/getUsersForChat', protect, getUsers)
-router.get('/profile', protect, getUserProfile)
-router.put('/profile', protect, updateUserProfile)
+router.get('/me', protect, getMyUser)
+router.put('/me', protect, updateMyUser)
+router.put('/me/username', protect, updateMyUsername)
+router.get('/:username', protect, getUserByUsername)
+
+// GET /me -> getMyUser
+// PUT /me/username -> changeMyUsername
+// PUT /me/password -> changeMyPassword
 
 export default router
